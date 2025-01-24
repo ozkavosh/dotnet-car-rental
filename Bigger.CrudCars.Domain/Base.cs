4adfcee5ead4f266a5d174c80c24cc9d0929ebe0
Ozkavosh <augustoslva@hotmail.com>
2025-01-24 16:10:43 -0300

    Update base entity with fields default value

--- Bigger.CrudCars.Domain/Base.cs
+++ Bigger.CrudCars.Domain/Base.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bigger.CrudCars.Domain
 {
     public class BaseEntity
     {
+        [Key]
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+
+        public BaseEntity() {
+            this.CreatedAt = DateTime.UtcNow;
+        }
     }
 }
