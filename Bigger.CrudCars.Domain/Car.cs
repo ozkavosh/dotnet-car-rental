1196763166fbe8e639d8864524b92a6ef811b87b
Ozkavosh <augustoslva@hotmail.com>
2025-01-24 09:56:31 -0300

    Add car entity

---
+++ Bigger.CrudCars.Domain/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace Bigger.CrudCars.Domain
+{
+    public class Car : BaseEntity
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
