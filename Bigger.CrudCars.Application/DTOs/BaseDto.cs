2dc6e0ba949277ce0e0b0808132ee36188e891d6
Ozkavosh <augustoslva@hotmail.com>
2025-01-24 16:09:37 -0300

    Create specific DTOs for car response and creation

---
+++ Bigger.CrudCars.Application/DTOs/BaseDto.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Bigger.CrudCars.Application.DTOs
+{
+    public class BaseDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("created-at")]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("updated-at")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
